5c5d690f2045e3769b0ade57e0259b29321ef705
luzihang123 <526154064@qq.com>
2021-04-20 17:53:09 +0800

    add:导包方式：匿名、别名、当前点

--- 8小时转职Golang/源代码/5.init/main.go
+++ 8小时转职Golang/源代码/5.init/main.go
@@ -8,11 +8,27 @@ package main
 ➜  cd src
 */
 import (
-	"GolangStudy/5.init/lib1"
-	"GolangStudy/5.init/lib2"
+	//"GolangStudy/5.init/lib1"
+	//"GolangStudy/5.init/lib2"
+
+	// 匿名导包,如果main函数中不使用，不会报错；无法使用当前包的方法，但是会执行当前包内部的init()方法
+	_ "GolangStudy/5.init/lib1"
+
+	// 起一个别名导包
+	//mylib2 "GolangStudy/5.init/lib2"
+
+	// (不建议使用：如果各个包有相同方法名称，会出现歧义)
+	//相当于，lib2包里的全部方法，导入本包的作用中，lib2中的全部方法可以直接使用API来调用，不需要lib2.API来调用
+	. "GolangStudy/5.init/lib2"
 )
 
 func main() {
-	lib1.Lib1Test()
-	lib2.Lib2Test()
+	//lib1.Lib1Test()
+	//lib2.Lib2Test()
+
+	// 起一个别名导包
+	//mylib2.Lib2Test()
+
+	// 相当于，lib2包里的方法名称， 属于当前包里的方法名
+	Lib2Test()
 }
