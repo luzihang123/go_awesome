a41d2f02c73807fd713cbf4f31074a7f59c76504
luzihang123 <526154064@qq.com>
2021-04-20 17:19:43 +0800

    默认从$GOPATH/src下导入,拷贝包代码到src文件夹下

--- 8小时转职Golang/源代码/5.init/main.go
+++ 8小时转职Golang/源代码/5.init/main.go
@@ -1,15 +1,15 @@
 package main
 
 /*
-默认从$GOPATH/src下导入
+默认从$GOPATH/src下导入,拷贝包代码到src文件夹下
 ➜  cd $GOPATH
 ➜  pwd
 /Users/luzihang/go
 ➜  cd src
 */
 import (
-	"源代码/5.init/lib1"
-	"源代码/5.init/lib2"
+	"GolangStudy/5.init/lib1"
+	"GolangStudy/5.init/lib2"
 )
 
 func main() {
